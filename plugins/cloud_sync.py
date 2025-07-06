f08d150892098478f16a6ab72f2a6427f6cf89c7
Charley <cryptosluss@gmail.com>
2025-07-06 10:41:57 -0700

    Restore clean cloud_sync plugin

--- plugins/cloud_sync.py
+++ plugins/cloud_sync.py
@@ -1,6 +1,8 @@
+# File: plugins/cloud_sync.py
+
 import time
 
 def run():
-    # Simulated cloud sync action
     with open("/home/charley/neuramatrix/memory/session_log.txt", "a") as log:
         log.write("[PLUGIN] Cloud Sync executed at: " + time.strftime("%Y-%m-%d %H:%M") + "\n")
+    print("[☁️ Cloud Sync] Simulated cloud sync complete.")
